ec0e0faec6e3b7a7ffc01e4f4f34602995b03140
Pierre-Leo Bourbonnais <leo.bourbonnais@polymtl.ca>
2013-11-29 16:40:16 -0500

    date_dimension model fill method

--- lib/generators/kg_datawarehouse/templates/date_dimensions/create_kg_datawarehouse_date_dimensions.rb
+++ lib/generators/kg_datawarehouse/templates/date_dimensions/create_kg_datawarehouse_date_dimensions.rb
@@ -1,13 +1,16 @@
 class CreateKgDatawarehouseDateDimensions < ActiveRecord::Migration
-  def self.up
+  def self.change
     create_table :kg_datawarehouse_date_dimensions, primary_key: :date_key  do |t|
       t.date      :full_date
       t.integer   :day_of_week
       t.integer   :day_num_in_month
+      t.integer   :day_num_in_year
       t.integer   :day_num_overall
       t.string    :day_name
       t.string    :day_abbrev
-      t.integer   :weekday_flag
+      t.string    :weekday_flag
+      t.boolean   :is_weekday
+      t.boolean   :is_weekend
       t.integer   :week_num_in_year
       t.integer   :week_num_overall
       t.date      :week_begin_date
@@ -22,16 +25,19 @@ class CreateKgDatawarehouseDateDimensions < ActiveRecord::Migration
       t.integer   :fiscal_month
       t.integer   :fiscal_quarter
       t.integer   :fiscal_year
-      t.boolean   :last_day_in_month_flag
+      t.string    :last_day_in_month_flag
       t.date      :same_day_year_ago_date
     end
     add_index :kg_datawarehouse_date_dimensions, :full_date, :name => :kg_dw_dd_full_date
     add_index :kg_datawarehouse_date_dimensions, :day_of_week, :name => :kg_dw_dd_day_of_week
     add_index :kg_datawarehouse_date_dimensions, :day_num_in_month, :name => :kg_dw_dd_day_num_in_month
+    add_index :kg_datawarehouse_date_dimensions, :day_num_in_year, :name => :kg_dw_dd_day_num_in_year
     add_index :kg_datawarehouse_date_dimensions, :day_num_overall, :name => :kg_dw_dd_day_num_overall
     add_index :kg_datawarehouse_date_dimensions, :day_name, :name => :kg_dw_dd_day_name
     add_index :kg_datawarehouse_date_dimensions, :day_abbrev, :name => :kg_dw_dd_day_abbrev
     add_index :kg_datawarehouse_date_dimensions, :weekday_flag, :name => :kg_dw_dd_weekday_flag
+    add_index :kg_datawarehouse_date_dimensions, :is_weekday, :name => :kg_dw_dd_is_weekday
+    add_index :kg_datawarehouse_date_dimensions, :is_weekend, :name => :kg_dw_dd_is_weekend
     add_index :kg_datawarehouse_date_dimensions, :week_num_in_year, :name => :kg_dw_dd_week_num_in_year
     add_index :kg_datawarehouse_date_dimensions, :week_num_overall, :name => :kg_dw_dd_week_num_overall
     add_index :kg_datawarehouse_date_dimensions, :week_begin_date, :name => :kg_dw_dd_week_begin_date
@@ -72,15 +78,13 @@ class CreateKgDatawarehouseDateDimensions < ActiveRecord::Migration
       t.integer   :minute
       t.string    :minute_zero_padded
       t.integer   :minutes_since_midnight
-      t.boolean   :first_minute_in_hour_flag
+      t.string    :first_minute_in_hour_flag
       t.integer   :second
       t.string    :second_zero_padded
       t.integer   :seconds_since_midnight
-      t.boolean   :first_second_in_hour_flag
-      t.boolean   :first_second_in_minute_flag
-      t.boolean   :am_flag
-      t.boolean   :pm_flag
-      t.string    :am_pm_marker
+      t.string    :first_second_in_hour_flag
+      t.string    :first_second_in_minute_flag
+      t.string    :am_pm
     end
     add_index :kg_datawarehouse_time_of_day_dimensions, :full_time_of_day, :name => :kg_dw_tod_last_day_in_month_flag
     add_index :kg_datawarehouse_time_of_day_dimensions, :hour_24, :name => :kg_dw_tod_hour_24
@@ -110,9 +114,7 @@ class CreateKgDatawarehouseDateDimensions < ActiveRecord::Migration
     add_index :kg_datawarehouse_time_of_day_dimensions, :seconds_since_midnight, :name => :kg_dw_tod_seconds_since_midnight
     add_index :kg_datawarehouse_time_of_day_dimensions, :first_second_in_hour_flag, :name => :kg_dw_tod_first_second_in_hour_flag
     add_index :kg_datawarehouse_time_of_day_dimensions, :first_second_in_minute_flag, :name => :kg_dw_tod_first_second_in_minute_flag
-    add_index :kg_datawarehouse_time_of_day_dimensions, :am_flag, :name => :kg_dw_tod_am_flag
-    add_index :kg_datawarehouse_time_of_day_dimensions, :pm_flag, :name => :kg_dw_tod_pm_flag
-    add_index :kg_datawarehouse_time_of_day_dimensions, :am_pm_marker, :name => :kg_dw_tod_am_pm_marker
+    add_index :kg_datawarehouse_time_of_day_dimensions, :am_pm, :name => :kg_dw_tod_am_pm
     
     create_table :kg_datawarehouse_hour_of_day_dimensions, primary_key: :hour_of_day_key  do |t|
       t.integer   :hour_24
@@ -123,9 +125,8 @@ class CreateKgDatawarehouseDateDimensions < ActiveRecord::Migration
       t.string    :hour_12_zero_padded
       t.integer   :minutes_since_midnight
       t.integer   :seconds_since_midnight
-      t.boolean   :am_flag
-      t.boolean   :pm_flag
-      t.string    :am_pm_marker
+
+      t.string    :am_pm
     end
     add_index :kg_datawarehouse_hour_of_day_dimensions, :hour_24, :name => :kg_dw_hod_hour_24
     add_index :kg_datawarehouse_hour_of_day_dimensions, :hour, :name => :kg_dw_hod_hour
@@ -135,71 +136,9 @@ class CreateKgDatawarehouseDateDimensions < ActiveRecord::Migration
     add_index :kg_datawarehouse_hour_of_day_dimensions, :hour_12_zero_padded, :name => :kg_dw_hod_hour_12_zero_padded
     add_index :kg_datawarehouse_hour_of_day_dimensions, :minutes_since_midnight, :name => :kg_dw_hod_minutes_since_midnight
     add_index :kg_datawarehouse_hour_of_day_dimensions, :seconds_since_midnight, :name => :kg_dw_hod_seconds_since_midnight
-    add_index :kg_datawarehouse_hour_of_day_dimensions, :am_flag, :name => :kg_dw_hod_am_flag
-    add_index :kg_datawarehouse_hour_of_day_dimensions, :pm_flag, :name => :kg_dw_hod_pm_flag
-    add_index :kg_datawarehouse_hour_of_day_dimensions, :am_pm_marker, :name => :kg_dw_hod_am_pm_marker
+    add_index :kg_datawarehouse_hour_of_day_dimensions, :am_pm, :name => :kg_dw_hod_am_pm_marker
   end
 
-  def self.down
-    remove_index :kg_datawarehouse_date_dimensions, :full_date
-    remove_index :kg_datawarehouse_date_dimensions, :day_of_week
-    remove_index :kg_datawarehouse_date_dimensions, :day_num_in_month
-    remove_index :kg_datawarehouse_date_dimensions, :day_num_overall
-    remove_index :kg_datawarehouse_date_dimensions, :day_name
-    remove_index :kg_datawarehouse_date_dimensions, :day_abbrev
-    remove_index :kg_datawarehouse_date_dimensions, :weekday_flag
-    remove_index :kg_datawarehouse_date_dimensions, :week_num_in_year
-    remove_index :kg_datawarehouse_date_dimensions, :week_num_overall
-    remove_index :kg_datawarehouse_date_dimensions, :week_begin_date
-    remove_index :kg_datawarehouse_date_dimensions, :week_begin_date_key
-    remove_index :kg_datawarehouse_date_dimensions, :month
-    remove_index :kg_datawarehouse_date_dimensions, :month_num_overall
-    remove_index :kg_datawarehouse_date_dimensions, :month_name
-    remove_index :kg_datawarehouse_date_dimensions, :month_abbrev
-    remove_index :kg_datawarehouse_date_dimensions, :quarter
-    remove_index :kg_datawarehouse_date_dimensions, :year
-    remove_index :kg_datawarehouse_date_dimensions, :yearmo
-    remove_index :kg_datawarehouse_date_dimensions, :fiscal_month
-    remove_index :kg_datawarehouse_date_dimensions, :fiscal_quarter
-    remove_index :kg_datawarehouse_date_dimensions, :fiscal_year
-    remove_index :kg_datawarehouse_date_dimensions, :last_day_in_month_flag
-    remove_index :kg_datawarehouse_date_dimensions, :same_day_year_ago_date
-    
-    drop_table :kg_datawarehouse_date_dimensions
-    
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :full_time_of_day
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_24
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_12
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_24_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_12_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_24
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_12
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_second_24
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_second
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_second_12
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_24_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_12_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_second_24_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_second_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :hour_minute_second_12_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :minute
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :minute_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :minutes_since_midnight
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :first_minute_in_hour_flag
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :second
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :second_zero_padded
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :seconds_since_midnight
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :first_second_in_hour_flag
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :first_second_in_minute_flag
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :am_flag
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :pm_flag
-    remove_index :kg_datawarehouse_time_of_day_dimensions, :am_pm_marker
-    
-    drop_table :kg_datawarehouse_time_of_day_dimensions
-  end
   
   
   
--- lib/models/kg_datawarehouse_date_dimension.rb
+++ lib/models/kg_datawarehouse_date_dimension.rb
@@ -1,7 +1,66 @@
+require "active_support/core_ext/numeric/time"
+
 module KgDatawarehouse
   class DateDimension < ActiveRecord::Base
     self.table_name = :kg_datawarehouse_date_dimensions
     self.primary_key = :date_key
     
+    def self.fill(args)
+      ActiveRecord::Base.transaction do
+        date_start = args.fetch(:start, nil)
+        date_end   = args.fetch(:end, nil)
+        date_start = args.fetch(:start, nil) unless date_start
+        date_end   = args.fetch(:end, nil)   unless date_end
+        date_start = Date.parse(date_start) unless date_start.respond_to?(:year) && date_start.respond_to?(:month) && date_start.respond_to?(:mday)
+        date_end   = Date.parse(date_end)   unless date_end.respond_to?(:year)   && date_end.respond_to?(:month)   && date_end.respond_to?(:mday)
+        i                 = 0
+        week_num          = 1
+        week_num_overall  = 1
+        month_num_overall = 1
+        last_date_year    = date_start.year
+        date_start.up_to(date_end) do |date|
+          new_date = self.new
+          new_date.date_key               = date.strftime("%Y%m%d").to_i
+          new_date.full_date              = date
+          new_date.day_of_week            = date.cwday
+          new_date.day_num_in_month       = date.day
+          new_date.day_num_in_year        = date.strftime("%j").to_i
+          new_date.day_num_overall        = i
+          new_date.day_name               = date.strftime("%A")
+          new_date.day_abbrev             = date.strftime("%a")
+          new_date.weekday_flag           = date.saturday? || date.sunday? ? "Weekend" : "Weekday"
+          new_date.is_weekday             = !(date.saturday? || date.sunday?)
+          new_date.is_weekend             = (date.saturday? || date.sunday?)
+          new_date.week_num_in_year       = week_num
+          new_date.week_num_overall       = week_num_overall
+          new_date.week_begin_date        = date.beginning_of_week
+          new_date.week_begin_date_key    = date.beginning_of_week.strftime("%Y%m%d").to_i
+          new_date.month                  = date.month
+          new_date.month_num_overall      = month_num_overall
+          new_date.month_name             = date.strftime("%B")
+          new_date.month_name_abbrev      = date.strftime("%b")
+          new_date.quarter                = ((date.month.to_f/12)*4).ceil
+          new_date.year                   = date.year
+          new_date.yearmo                 = date;strftiime("%Y%m").to_i
+          new_date.fiscal_month           = nil
+          new_date.fiscal_quarter         = nil
+          new_date.fiscal_year            = nil
+          new_date.last_day_in_month_flag = (date+1.day).day == 1 ? "Month End" : "Not Month End"
+          new_date.is_last_day_in_month   = ((date+1.day).day == 1)
+          new_date.same_day_year_ago_date = (date.month == 2 && date.day == 29) ? ((date-1.year)+1.day) : (date-1.year) # make sure we fall on March 1 if date is February 29.
+          
+          i                 += 1
+          week_num          += 1 if date.sunday?
+          week_num_overall  += 1 if date.sunday?
+          month_num_overall += 1 if date.day == 1
+          
+          if last_date_year != new_date.year
+            last_date_year  = new_date.year
+            week_num        = 1 # reset week num if we change year
+          end
+          
+        end
+      end
+    end
   end
 end
